6da823450829a84bb165c17fc4dafa305a862c21
unknown <opokudavid446@gmail.com>
2020-08-20 12:09:22 +0100

    Init commit

--- App.tsx
+++ App.tsx
@@ -1,23 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
   } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
+    return <SafeAreaProvider></SafeAreaProvider>;
   }
 }
--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,24 +1,26 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import * as React from 'react';
+import { Ionicons, MaterialCommunityIcons, Feather } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import * as React from "react";
 
 export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
 
-  // Load any resources or data that we need prior to rendering the app
   React.useEffect(() => {
     async function loadResourcesAndDataAsync() {
       try {
         SplashScreen.preventAutoHideAsync();
 
-        // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
-          'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          ...MaterialCommunityIcons.font,
+          ...Feather.font,
+          "SFProDisplay-Bold": require("../assets/fonts/SFProDisplay-Bold.ttf"),
+          "SFProDisplay-Regular": require("../assets/fonts/SFProDisplay-Regular.ttf"),
+          "SFProDisplay-SemiBold": require("../assets/fonts/SFProDisplay-SemiBold.ttf"),
+          "SFProDisplay-Medium": require("../assets/fonts/SFProDisplay-Medium.ttf"),
         });
       } catch (e) {
-        // We might want to provide this error information to an error reporting service
         console.warn(e);
       } finally {
         setLoadingComplete(true);
