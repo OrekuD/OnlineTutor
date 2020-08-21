1efe9bb96b227c9621a4a09f23f3337c61816d7b
unknown <opokudavid446@gmail.com>
2020-08-21 17:41:33 +0100

    :wrench:

--- screens/Mentor.tsx
+++ screens/Mentor.tsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { View, StyleSheet, ScrollView, Image } from "react-native";
 import { Text, Button, ReviewCard, DetailsCard } from "../components";
 import { StackScreenProps } from "@react-navigation/stack";
 import { RootStackParamList } from "../types";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { palewhite, black, white, grey } from "../constants/Colors";
+import { palewhite, black, white, grey, lightgrey } from "../constants/Colors";
 import { BorderlessButton } from "react-native-gesture-handler";
 import { Feather, Octicons } from "@expo/vector-icons";
 import { height, width } from "../constants/Layout";
 import { reviews } from "../data/reviews";
+import BottomSheetBehavior from "reanimated-bottom-sheet";
 
 const scale = height > 700 ? 1.4 : 1.2;
+
+const renderContent = () => (
+  <View style={styles.modalContent}>
+    <View style={styles.knob} />
+    <Text variant="caption" style={{ alignSelf: "center" }}>
+      Schedule a class
+    </Text>
+    <View style={styles.section}>
+      <Text variant="caption">Select day</Text>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Text
+          variant="caption"
+          color="grey"
+          style={{ marginRight: 3, marginBottom: 3 }}
+        >
+          July
+        </Text>
+        <Feather name="chevron-down" color={grey} size={28} />
+      </View>
+    </View>
+    <View style={{ ...styles.buttonContainer }}>
+      <Button
+        label="Continue to payment"
+        // onPress={() => setIsVisble(true)}
+      />
+    </View>
+  </View>
+);
 
 const Mentor = ({
   navigation,
@@ -19,11 +48,15 @@ const Mentor = ({
   const {
     item: { image, name, experience, price, rating, role, about },
   } = route.params;
-  const { top: height, bottom: paddingBottom } = useSafeAreaInsets();
+  const { top, bottom: paddingBottom } = useSafeAreaInsets();
+  const [isVisible, setIsVisble] = useState<boolean>(false);
+  const bottomRef = useRef<BottomSheetBehavior>();
+
+  const snapPoints = [0, height * 0.4, height * 0.7];
 
   return (
-    <>
-      <View style={{ height, backgroundColor: palewhite }} />
+    <View style={{ flex: 1 }}>
+      <View style={{ height: top, backgroundColor: palewhite }} />
       <ScrollView style={{ backgroundColor: palewhite }}>
         <BorderlessButton
           onPress={navigation.goBack}
@@ -78,9 +111,18 @@ const Mentor = ({
         </View>
       </ScrollView>
       <View style={{ ...styles.buttonContainer, paddingBottom }}>
-        <Button label="Schedule a class" />
+        <Button
+          label="Schedule a class"
+          onPress={() => bottomRef.current?.snapTo(2)}
+        />
       </View>
-    </>
+      <BottomSheetBehavior
+        snapPoints={snapPoints}
+        renderContent={renderContent}
+        initialSnap={0}
+        ref={bottomRef}
+      />
+    </View>
   );
 };
 
@@ -127,5 +169,31 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     marginVertical: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    margin: 0,
+  },
+  modalContent: {
+    height: height * 0.7,
+    width: width,
+    backgroundColor: palewhite,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  knob: {
+    width: 50,
+    height: 5,
+    backgroundColor: grey,
+    borderRadius: 3,
+    marginVertical: 15,
+    alignSelf: "center",
+  },
+  section: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: 15,
   },
 });
